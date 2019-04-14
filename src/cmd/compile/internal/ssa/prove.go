197adbf78ad6dd152c583fb741a08355e8e18b75
zdjones <zachj1@gmail.com>
2019-04-14 18:01:26 +0100

    move len <= cap ops to prove from update
    Change-Id: I7b1da1ecded4a615451fdecf7a1ded28373f27da

--- src/cmd/compile/internal/ssa/prove.go
+++ src/cmd/compile/internal/ssa/prove.go
@@ -387,31 +387,31 @@ func (ft *factsTable) update(parent *Block, v, w *Value, d domain, r relation) {
 		return
 	}
 
-	// Additional facts we know given the relationship between len and cap.
-	//
-	// TODO: Since prove now derives transitive relations, it
-	// should be sufficient to learn that len(w) <= cap(w) at the
-	// beginning of prove where we look for all len/cap ops.
-	if v.Op == OpSliceLen && r&lt == 0 && ft.caps[v.Args[0].ID] != nil {
-		// len(s) > w implies cap(s) > w
-		// len(s) >= w implies cap(s) >= w
-		// len(s) == w implies cap(s) >= w
-		ft.update(parent, ft.caps[v.Args[0].ID], w, d, r|gt)
-	}
-	if w.Op == OpSliceLen && r&gt == 0 && ft.caps[w.Args[0].ID] != nil {
-		// same, length on the RHS.
-		ft.update(parent, v, ft.caps[w.Args[0].ID], d, r|lt)
-	}
-	if v.Op == OpSliceCap && r&gt == 0 && ft.lens[v.Args[0].ID] != nil {
-		// cap(s) < w implies len(s) < w
-		// cap(s) <= w implies len(s) <= w
-		// cap(s) == w implies len(s) <= w
-		ft.update(parent, ft.lens[v.Args[0].ID], w, d, r|lt)
-	}
-	if w.Op == OpSliceCap && r&lt == 0 && ft.lens[w.Args[0].ID] != nil {
-		// same, capacity on the RHS.
-		ft.update(parent, v, ft.lens[w.Args[0].ID], d, r|gt)
-	}
+	// // Additional facts we know given the relationship between len and cap.
+	// //
+	// // TODO: Since prove now derives transitive relations, it
+	// // should be sufficient to learn that len(w) <= cap(w) at the
+	// // beginning of prove where we look for all len/cap ops.
+	// if v.Op == OpSliceLen && r&lt == 0 && ft.caps[v.Args[0].ID] != nil {
+	// 	// len(s) > w implies cap(s) > w
+	// 	// len(s) >= w implies cap(s) >= w
+	// 	// len(s) == w implies cap(s) >= w
+	// 	ft.update(parent, ft.caps[v.Args[0].ID], w, d, r|gt)
+	// }
+	// if w.Op == OpSliceLen && r&gt == 0 && ft.caps[w.Args[0].ID] != nil {
+	// 	// same, length on the RHS.
+	// 	ft.update(parent, v, ft.caps[w.Args[0].ID], d, r|lt)
+	// }
+	// if v.Op == OpSliceCap && r&gt == 0 && ft.lens[v.Args[0].ID] != nil {
+	// 	// cap(s) < w implies len(s) < w
+	// 	// cap(s) <= w implies len(s) <= w
+	// 	// cap(s) == w implies len(s) <= w
+	// 	ft.update(parent, ft.lens[v.Args[0].ID], w, d, r|lt)
+	// }
+	// if w.Op == OpSliceCap && r&lt == 0 && ft.lens[w.Args[0].ID] != nil {
+	// 	// same, capacity on the RHS.
+	// 	ft.update(parent, v, ft.lens[w.Args[0].ID], d, r|gt)
+	// }
 
 	// Process fence-post implications.
 	//
@@ -762,6 +762,20 @@ func prove(f *Func) {
 				ft.caps[v.Args[0].ID] = v
 				ft.update(b, v, ft.zero, signed, gt|eq)
 			}
+		}
+	}
+
+	// TODO: Since prove now derives transitive relations, it
+	// should be sufficient to learn that len(w) <= cap(w) at the
+	// beginning of prove where we look for all len/cap ops.
+	if ft.caps != nil && ft.lens != nil {
+		for sID, c := range ft.caps {
+			// Len should always exist, but check to be sure
+			if l, has := ft.lens[sID]; has {
+				// TODO(zdjones): Should the block be from l, or c?
+				addRestrictions(c.Block, ft, signed|unsigned, l, c, lt|eq)
+			}
+
 		}
 	}
 
